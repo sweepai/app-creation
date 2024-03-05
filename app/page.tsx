fdcf40b913dc966a7ed773c937aae0e0b0746654
Kevin Lu <kevinlu1248@gmail.com>
2024-03-04 18:59:36 -0800

    Init MVP

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,123 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Image from "next/image";
+import { useState } from "react";
+
+const DEFAULT_CONFIG = {
+      name: "sweep-ai-self-hosted",
+      url: "https://docs.sweep.dev/usage/deployment",
+      public: true,
+      default_permissions: {
+        administration: "read",
+        actions: "read",
+        checks: "read",
+        contents: "write",
+        issues: "write",
+        metadata: "read",
+        pull_requests: "write",
+        statuses: "read",
+        workflows: "write"
+      },
+      default_events: [
+        "check_run",
+        "check_suite",
+        "commit_comment",
+        "issue_comment",
+        "issues",
+        "label",
+        "pull_request",
+        "pull_request_review",
+        "pull_request_review_comment",
+        "pull_request_review_thread",
+        "push",
+        "status",
+        "workflow_job",
+        "workflow_run"
+      ],
+      redirect_url: "http://localhost:3000/redirect",
+      hook_attributes: {
+        url: "https://example.com/github/events",
+      },
+    }
+  
+const DEFAULT_CONFIG_STRING = JSON.stringify(DEFAULT_CONFIG)
 
 export default function Home() {
+  const [organizationName, setOrganizationName] = useState("");
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+    <main className="flex min-h-screen flex-col items-center justify-around p-24">
+      {/* <div className="relative flex place-items-center before:absolute before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]"> */}
+      {/* <div>
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
-          height={37}
+          height={10}
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
+      </div> */}
+      <Tabs defaultValue="individual" className="w-[600px]">
+        <a href="https://docs.sweep.dev/deployment">
+          <div className="flex justify-around mb-16">
+            <img src="sweeping.gif" alt="Sweep's logo" className="rounded-full bg-zinc-900 h-[250px] w-[250px]"/>
+          </div>
         </a>
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="individual">Individual</TabsTrigger>
+          <TabsTrigger value="organization">Organization</TabsTrigger>
+        </TabsList>
+        <TabsContent value="individual" >
+          <Card className="p-4">
+            <CardHeader>
+              <CardTitle>Individual GitHub Account</CardTitle>
+              <CardDescription>
+                Register a self-hosted Sweep GitHub App under your own account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <form action="https://github.com/settings/apps/new" method="post">
+                <input type="text" name="manifest" id="manifest" value={DEFAULT_CONFIG_STRING} hidden/>
+                <br/>
+                <Button type="submit" className="bg-blue-800 hover:bg-blue-900 text-white">
+                  Create App
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+        <TabsContent value="organization">
+          <Card className="p-4">
+            <CardHeader>
+              <CardTitle>Organization GitHub Account</CardTitle>
+              <CardDescription>
+                Register a self-hosted Sweep GitHub App under an organization account.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Label>Organization Name</Label>
+              <Input type="text" placeholder="Name of organization e.g. 'sweepai'" value={organizationName} onChange={(e) => {
+                setOrganizationName(e.target.value)}
+              } />
+              <form action={`https://github.com/organizations/${organizationName}/settings/apps/new`} method="post">
+                <input type="text" name="manifest" id="manifest" value={DEFAULT_CONFIG_STRING} hidden/>
+                <br/>
+                <Button type="submit" className="bg-blue-800 hover:bg-blue-900 text-white">
+                  Create App
+                </Button>
+              </form>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
